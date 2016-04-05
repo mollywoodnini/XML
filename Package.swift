0ab7e7c825a262cc3b9306d8d5a21034e0b657bc
badim <goloveychuk@mail.ru>
2016-04-05 15:29:53 +0300

    including clibxml2

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "XML",
+    targets: [Target(name: "Xml", dependencies: ["CLibXML2"])],
     dependencies: [
         .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/CLibXML2.git", majorVersion: 0, minor: 4)
     ]
 )
