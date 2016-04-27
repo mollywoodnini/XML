4bed1757d15df5d7c1cc0df771450fd416c0dfde
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-27 01:59:39 -0300

    add travis

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "XML",
-    targets: [Target(name: "Xml", dependencies: ["CLibXML2"])],
     dependencies: [
         .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/CLibXML2.git", majorVersion: 0, minor: 5),
     ]
 )
